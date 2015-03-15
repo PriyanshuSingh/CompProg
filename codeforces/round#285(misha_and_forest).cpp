5cacff41acf8fb7eca65797f8ac173240260140c
PriyanshuSingh <priyanshusingh05101994@gmail.com>
2015-03-15 17:09:17 +0530

    div 1 round 285

---
+++ codeforces/round#285(misha_and_forest).cpp
@@ -0,0 +1,128 @@
+/************************************************************
+ * Author: Priyanshu Singh
+ * Handle: tgamerz
+ */
+
+
+#include <cmath>
+#include <climits>
+#include <queue>
+#include <vector>
+#include <map>
+#include <cstdlib>
+#include <fstream>
+#include <iomanip>   
+#include <iostream>  
+#include <sstream> 
+#include <stack>
+#include <algorithm>
+#include <cstring>
+#include <cassert>
+
+using namespace std;
+// Input macros
+#define s(n)                        scanf("%d",&n)
+#define sc(n)                       scanf("%c",&n)
+#define sl(n)                       scanf("%lld",&n)
+#define sf(n)                       scanf("%f",&n)
+#define slf(n)                      scanf("%lf",&n)
+#define ss(n)                       scanf("%s",n)
+
+// Useful constants
+#define INF                         (int)1e9
+#define EPS                         1e-9
+
+// Useful hardware instructions
+#define bitcount                    __builtin_popcount
+#define gcd                         __gcd
+
+// Useful container manipulation / traversal macros
+#define forall(i,a,b)               for(int i=a;i<b;i++)
+#define foreach(v, c)               for( typeof( (c).begin()) v = (c).begin();  v != (c).end(); ++v)
+#define all(a)                      a.begin(), a.end()
+#define in(a,b)                     ( (b).find(a) != (b).end())
+#define pb                          push_back
+#define fill(a,v)                    memset(a, v, sizeof a)
+#define sz(a)                       ((int)(a.size()))
+#define mp                          make_pair
+#define F                           first
+#define S                           second
+
+// Some common useful functions
+#define maX(a,b)                     ( (a) > (b) ? (a) : (b))
+#define miN(a,b)                     ( (a) < (b) ? (a) : (b))
+#define checkbit(n,b)                ( (n >> b) & 1)
+#define DREP(a)                      sort(all(a)); a.erase(unique(all(a)),a.end())
+#define INDEX(arr,ind)               (lower_bound(all(arr),ind)-arr.begin())
+
+#define DEBUG
+
+// debugging
+#ifdef DEBUG
+#define trace1(x)                    cerr << #x << ": " << x << endl;
+#define trace2(x, y)                 cerr << #x << ": " << x << " | " << #y << ": " << y << endl;
+#define trace3(x, y, z)              cerr << #x << ": " << x << " | " << #y << ": " << y << " | " << #z << ": " << z << endl;
+#define trace4(a, b, c, d)           cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << endl;
+#define trace5(a, b, c, d, e)        cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << " | " << #e << ": " << e << endl;
+#define trace6(a, b, c, d, e, f)     cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << " | " << #e << ": " << e << " | " << #f << ": " << f << endl;
+#else
+#define trace1(x)
+#define trace2(x, y)
+#define trace3(x, y, z)
+#define trace4(a, b, c, d)
+#define trace5(a, b, c, d, e)
+#define trace6(a, b, c, d, e, f)
+#endif
+
+
+// datatypes
+#define ll long long
+#define ull unsigned long long
+#define ui unsigned int
+#define us unsigned short
+#define gc  getchar_unlocked
+
+/********************** // don't know why getchar_unlocked is giving compilation error on codeforces
+inline void fs(int &x)
+{
+    register int c = gc();
+    x = 0;
+    int neg = 0;
+    for(;((c<48 || c>57) && c != '-');c = gc());
+    if(c=='-') {neg=1;c=gc();}
+    for(;c>47 && c<58;c = gc()) {x = (x<<1) + (x<<3) + c - 48;}
+    if(neg) x=-x;
+}
+*/
+
+
+pair<int, int> info[1<<16];
+queue<int> Q;
+
+int main(){
+    int n,d,sm,edge;
+    s(n);
+    edge = 0;
+    forall(i, 0, n){
+        s(d);s(sm);
+        info[i] = mp(d,sm);
+        edge += d;
+        if(d == 1)Q.push(i);
+    }
+    printf("%d\n",edge/2);
+    while(!Q.empty()){
+        int v = Q.front();
+        Q.pop();
+        if(info[v].F == 0)continue;
+        int p = info[v].S;
+        printf("%d %d\n",p,v);
+        info[p].F--;
+        info[p].S ^= v;
+        if(info[p].F == 1)Q.push(p);
+    }
+    return 0;
+}
+
+
+
+
