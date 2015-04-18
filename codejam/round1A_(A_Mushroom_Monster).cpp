0b1e37ac383fbdc8643d39a386574b2265beddff
PriyanshuSingh <priyanshusingh05101994@gmail.com>
2015-04-18 10:28:17 +0530

    round 1 A codejam 2015

---
+++ codejam/round1A_(A_Mushroom_Monster).cpp
@@ -0,0 +1,113 @@
+/************************************************************
+ * Author: Priyanshu Singh
+ * Handle: tgamerz
+ */
+
+#include <cmath>
+#include <climits>
+#include <queue>
+#include <vector>
+#include <map>
+#include <cstdlib>
+#include <fstream>
+#include <iomanip>   
+#include <iostream>  
+#include <sstream> 
+#include <stack>
+#include <algorithm>
+#include <cstring>
+#include <cassert>
+#include <bitset>
+
+using namespace std;
+// Input macros
+#define s(n)                        scanf("%d",&n)
+#define sc(n)                       scanf("%c",&n)
+#define sl(n)                       scanf("%lld",&n)
+#define sf(n)                       scanf("%f",&n)
+#define slf(n)                      scanf("%lf",&n)
+#define ss(n)                       scanf("%s",n)
+
+// Useful constants
+#define INF                         (int)1e9
+#define EPS                         1e-9
+
+// Useful hardware instructions
+#define bitcount                    __builtin_popcount
+#define gcd                         __gcd
+
+// Useful container manipulation / traversal macros
+#define forall(i,a,b)               for(int i=a;i<b;i++)
+#define foreach(v, c)               for( typeof( (c).begin()) v = (c).begin();  v != (c).end(); ++v)
+#define all(a)                      a.begin(), a.end()
+#define in(a,b)                     ( (b).find(a) != (b).end())
+#define pb                          push_back
+#define fill(a,v)                    memset(a, v, sizeof a)
+#define sz(a)                       ((int)(a.size()))
+#define mp                          make_pair
+#define F                           first
+#define S                           second
+
+// Some common useful functions
+#define maX(a,b)                     ( (a) > (b) ? (a) : (b))
+#define miN(a,b)                     ( (a) < (b) ? (a) : (b))
+#define checkbit(n,b)                ( (n >> b) & 1)
+#define DREP(a)                      sort(all(a)); a.erase(unique(all(a)),a.end())
+#define INDEX(arr,ind)               (lower_bound(all(arr),ind)-arr.begin())
+
+#define DEBUG
+
+// debugging
+#ifdef DEBUG
+#define trace1(x)                    cerr << #x << ": " << x << endl;
+#define trace2(x, y)                 cerr << #x << ": " << x << " | " << #y << ": " << y << endl;
+#define trace3(x, y, z)              cerr << #x << ": " << x << " | " << #y << ": " << y << " | " << #z << ": " << z << endl;
+#define trace4(a, b, c, d)           cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << endl;
+#define trace5(a, b, c, d, e)        cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << " | " << #e << ": " << e << endl;
+#define trace6(a, b, c, d, e, f)     cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << " | " << #e << ": " << e << " | " << #f << ": " << f << endl;
+#else
+#define trace1(x)
+#define trace2(x, y)
+#define trace3(x, y, z)
+#define trace4(a, b, c, d)
+#define trace5(a, b, c, d, e)
+#define trace6(a, b, c, d, e, f)
+#endif
+
+
+// datatypes
+#define ll long long
+#define ull unsigned long long
+#define ui unsigned int
+#define us unsigned short
+
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+typedef pair< pii, int> piii;
+typedef vector< piii > vpiii;
+typedef pair<int, bool> pib;
+typedef vector< pii > vpii;
+typedef vector< pib > vpib;
+
+int main(){
+    int t,n,pn[1010],x,y,z;
+    s(t);
+    forall(i, 1, t+1){
+        s(n);
+        s(pn[0]);
+        x = 0;
+        y = 0;
+        z = 0;
+        forall(j, 1, n){
+            s(pn[j]);
+            if(pn[j] < pn[j-1]){
+                x += pn[j-1] - pn[j];
+                y = maX(y, pn[j-1] - pn[j]);
+            }
+        }
+        forall(j, 0, n-1){
+            z += miN(pn[j], y);
+        }
+        printf("Case #%d: %d %d\n",i,x,z);
+    }
+}
---
+++ codejam/round1A_(B_Haircut).cpp
@@ -0,0 +1,137 @@
+/************************************************************
+ * Author: Priyanshu Singh
+ * Handle: tgamerz
+ */
+
+#include <cmath>
+#include <climits>
+#include <queue>
+#include <vector>
+#include <map>
+#include <cstdlib>
+#include <fstream>
+#include <iomanip>   
+#include <iostream>  
+#include <sstream> 
+#include <stack>
+#include <algorithm>
+#include <cstring>
+#include <cassert>
+#include <bitset>
+
+using namespace std;
+// Input macros
+#define s(n)                        scanf("%d",&n)
+#define sc(n)                       scanf("%c",&n)
+#define sl(n)                       scanf("%lld",&n)
+#define sf(n)                       scanf("%f",&n)
+#define slf(n)                      scanf("%lf",&n)
+#define ss(n)                       scanf("%s",n)
+
+// Useful constants
+#define INF                         (int)1e9
+#define EPS                         1e-9
+
+// Useful hardware instructions
+#define bitcount                    __builtin_popcount
+#define gcd                         __gcd
+
+// Useful container manipulation / traversal macros
+#define forall(i,a,b)               for(int i=a;i<b;i++)
+#define foreach(v, c)               for( typeof( (c).begin()) v = (c).begin();  v != (c).end(); ++v)
+#define all(a)                      a.begin(), a.end()
+#define in(a,b)                     ( (b).find(a) != (b).end())
+#define pb                          push_back
+#define fill(a,v)                    memset(a, v, sizeof a)
+#define sz(a)                       ((int)(a.size()))
+#define mp                          make_pair
+#define F                           first
+#define S                           second
+
+// Some common useful functions
+#define maX(a,b)                     ( (a) > (b) ? (a) : (b))
+#define miN(a,b)                     ( (a) < (b) ? (a) : (b))
+#define checkbit(n,b)                ( (n >> b) & 1)
+#define DREP(a)                      sort(all(a)); a.erase(unique(all(a)),a.end())
+#define INDEX(arr,ind)               (lower_bound(all(arr),ind)-arr.begin())
+
+#define DEBUG
+
+// debugging
+#ifdef DEBUG
+#define trace1(x)                    cerr << #x << ": " << x << endl;
+#define trace2(x, y)                 cerr << #x << ": " << x << " | " << #y << ": " << y << endl;
+#define trace3(x, y, z)              cerr << #x << ": " << x << " | " << #y << ": " << y << " | " << #z << ": " << z << endl;
+#define trace4(a, b, c, d)           cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << endl;
+#define trace5(a, b, c, d, e)        cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << " | " << #e << ": " << e << endl;
+#define trace6(a, b, c, d, e, f)     cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << " | " << #e << ": " << e << " | " << #f << ": " << f << endl;
+#else
+#define trace1(x)
+#define trace2(x, y)
+#define trace3(x, y, z)
+#define trace4(a, b, c, d)
+#define trace5(a, b, c, d, e)
+#define trace6(a, b, c, d, e, f)
+#endif
+
+
+// datatypes
+#define ll long long
+#define ull unsigned long long
+#define ui unsigned int
+#define us unsigned short
+
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+typedef pair< pii, int> piii;
+typedef vector< piii > vpiii;
+typedef pair<int, bool> pib;
+typedef vector< pii > vpii;
+typedef vector< pib > vpib;
+
+int main(){
+    ll t,N,B,M[1001],temp_ans,temp,count;
+    sl(t);
+    forall(i, 1, t+1){
+        sl(B);sl(N);
+        int mx = 0;
+        forall(j, 0, B){
+            sl(M[j]);
+            if(mx < M[j])mx = M[j];
+        }
+        
+        if(N <= B){
+            printf("Case #%d: %lld\n",i,N);
+        }else{
+            ll left = 0;
+            ll right = N*mx;
+            vi same;
+            while(left <= right){
+                ll mid = (left+right)>>1;
+                same.clear();
+                count = 0;
+                temp = 0;
+                forall(j, 0, B){
+                    temp += mid/M[j] + 1;
+                    if(mid % M[j] == 0){
+                        count++;
+                        same.pb(j);
+                    }
+                }
+                //trace5(left, right, mid, count, temp)
+                if(N > temp - count && temp >= N && count > 0)break;
+                else if(temp >= N){
+                    right = mid-1;
+                }else{
+                    left = mid+1;
+                }
+            }
+            count =  temp - count + 1;
+            foreach(v, same){
+                if(count == N)temp_ans=*v;
+                count++;
+            }
+            printf("Case #%d: %lld\n",i,temp_ans+1);
+        }
+    }
+}
