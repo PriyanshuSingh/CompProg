a2a51c0a4a1cee4b68859510040747396d7df56f
PriyanshuSingh <priyanshusingh05101994@gmail.com>
2015-05-05 01:50:57 +0530

    vk cup 3 Problem C

---
+++ codeforces/vk_cup_round3_C.cpp
@@ -0,0 +1,153 @@
+/************************************************************
+ * Author: Priyanshu Singh
+ * Handle: tgamerz
+ */
+
+#include <cmath>
+#include <climits>
+#include <queue>
+#include <vector>
+#include <map>
+#include <cstdlib>
+#include <fstream>
+#include <iomanip>   
+#include <iostream>  
+#include <sstream> 
+#include <stack>
+#include <algorithm>
+#include <cstring>
+#include <cassert>
+#include <bitset>
+
+using namespace std;
+// Input macros
+#define s(n)                        scanf("%d",&n)
+#define sc(n)                       scanf("%c",&n)
+#define sl(n)                       scanf("%lld",&n)
+#define sf(n)                       scanf("%f",&n)
+#define slf(n)                      scanf("%lf",&n)
+#define ss(n)                       scanf("%s",n)
+
+// Useful constants
+#define INF                         (int)1e9
+#define EPS                         1e-9
+
+// Useful hardware instructions
+#define bitcount                    __builtin_popcount
+#define gcd                         __gcd
+
+// Useful container manipulation / traversal macros
+#define forall(i,a,b)               for(int i=a;i<b;i++)
+#define foreach(v, c)               for( typeof( (c).begin()) v = (c).begin();  v != (c).end(); ++v)
+#define all(a)                      a.begin(), a.end()
+#define in(a,b)                     ( (b).find(a) != (b).end())
+#define pb                          push_back
+#define fill(a,v)                    memset(a, v, sizeof a)
+#define sz(a)                       ((int)(a.size()))
+#define mp                          make_pair
+#define F                           first
+#define S                           second
+
+// Some common useful functions
+#define maX(a,b)                     ( (a) > (b) ? (a) : (b))
+#define miN(a,b)                     ( (a) < (b) ? (a) : (b))
+#define checkbit(n,b)                ( (n >> b) & 1)
+#define DREP(a)                      sort(all(a)); a.erase(unique(all(a)),a.end())
+#define INDEX(arr,ind)               (lower_bound(all(arr),ind)-arr.begin())
+
+#define DEBUG
+
+// debugging
+#ifdef DEBUG
+#define trace1(x)                    cerr << #x << ": " << x << endl;
+#define trace2(x, y)                 cerr << #x << ": " << x << " | " << #y << ": " << y << endl;
+#define trace3(x, y, z)              cerr << #x << ": " << x << " | " << #y << ": " << y << " | " << #z << ": " << z << endl;
+#define trace4(a, b, c, d)           cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << endl;
+#define trace5(a, b, c, d, e)        cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << " | " << #e << ": " << e << endl;
+#define trace6(a, b, c, d, e, f)     cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << " | " << #e << ": " << e << " | " << #f << ": " << f << endl;
+#else
+#define trace1(x)
+#define trace2(x, y)
+#define trace3(x, y, z)
+#define trace4(a, b, c, d)
+#define trace5(a, b, c, d, e)
+#define trace6(a, b, c, d, e, f)
+#endif
+
+//dirextion
+const int fx[4][2] = {{0,1}, {0,-1}, {1,0}, {-1,0}};
+const int fxx[8][2] = {{0,1}, {0,-1}, {1,0}, {-1,0}, {1,1}, {1,-1}, {-1,1}, {-1,-1}};
+
+// datatypes
+#define ll long long
+#define ull unsigned long long
+#define ui unsigned int
+#define us unsigned short
+
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+typedef pair< pii, int> piii;
+typedef vector< piii > vpiii;
+typedef pair<int, bool> pib;
+typedef vector< pii > vpii;
+typedef vector< pib > vpib;
+
+const int MAXN = 210;
+int f[MAXN];
+ll chain[MAXN];
+ll length[MAXN];
+
+int temp[MAXN];
+void find_k(int i){
+    int k = 1;
+    int ini = i;
+    vi v;
+    if(f[i] == i){
+        chain[i] = 1;
+        length[i] = 0;
+        return;
+    }
+    fill(temp, 0);
+    temp[ini] = k;
+    v.pb(ini);
+    while(true){
+        k++;
+        i = f[i];
+        if(find(all(v),i) != v.end()){
+            chain[ini] = k - temp[i];
+            length[ini] = temp[i] - 1;
+            return;
+        }else{
+            v.pb(i);
+            temp[i] = k;
+        }
+    }
+}
+
+int main(){
+    int N;
+    s(N);
+    forall(i, 1, N+1){
+        s(f[i]);
+    }
+    int mx = 1;
+    forall(i, 1, N+1){
+        find_k(i);
+ //       trace2(i,f[i])
+    }
+    ll ans = 1;
+    bool flag = false;
+    int val = 1;
+    forall(i, 1, N+1){
+        //trace3(i, chain[i], length[i])
+        if(chain[i] != 0){
+            if(ans % chain[i] != 0)ans = (ans * chain[i] / gcd(ans,chain[i]));
+            flag = true;
+        }
+        val = maX(val, length[i]);
+    }
+    int mul =1 ;
+    if(ans < val)while(ans * mul < val)mul++;
+    printf("%lld\n",ans * mul);
+    return 0;
+}
