ce02e22b0f575297b2a918d68091f911d69f03d4
PriyanshuSingh <priyanshusingh05101994@gmail.com>
2015-05-05 21:05:32 +0530

    vk cup round 3 problem F

---
+++ codeforces/vk_cup_round3_F.cpp
@@ -0,0 +1,139 @@
+/************************************************************
+ * Author: Priyanshu Singh
+ * Handle: tgamerz
+ */
+
+#include <cmath>
+#include <climits>
+#include <queue>
+#include <vector>
+#include <map>
+#include <cstdlib>
+#include <fstream>
+#include <iomanip>   
+#include <iostream>  
+#include <sstream> 
+#include <stack>
+#include <algorithm>
+#include <cstring>
+#include <cassert>
+#include <bitset>
+
+using namespace std;
+// Input macros
+#define s(n)                        scanf("%d",&n)
+#define sc(n)                       scanf("%c",&n)
+#define sl(n)                       scanf("%lld",&n)
+#define sf(n)                       scanf("%f",&n)
+#define slf(n)                      scanf("%lf",&n)
+#define ss(n)                       scanf("%s",n)
+
+// Useful constants
+#define INF                         (int)1e9
+#define EPS                         1e-9
+
+// Useful hardware instructions
+#define bitcount                    __builtin_popcount
+#define gcd                         __gcd
+
+// Useful container manipulation / traversal macros
+#define forall(i,a,b)               for(int i=a;i<b;i++)
+#define foreach(v, c)               for( typeof( (c).begin()) v = (c).begin();  v != (c).end(); ++v)
+#define all(a)                      a.begin(), a.end()
+#define in(a,b)                     ( (b).find(a) != (b).end())
+#define pb                          push_back
+#define fill(a,v)                    memset(a, v, sizeof a)
+#define sz(a)                       ((int)(a.size()))
+#define mp                          make_pair
+#define F                           first
+#define S                           second
+
+// Some common useful functions
+#define maX(a,b)                     ( (a) > (b) ? (a) : (b))
+#define miN(a,b)                     ( (a) < (b) ? (a) : (b))
+#define checkbit(n,b)                ( (n >> b) & 1)
+#define DREP(a)                      sort(all(a)); a.erase(unique(all(a)),a.end())
+#define INDEX(arr,ind)               (lower_bound(all(arr),ind)-arr.begin())
+
+#define DEBUG
+
+// debugging
+#ifdef DEBUG
+#define trace1(x)                    cerr << #x << ": " << x << endl;
+#define trace2(x, y)                 cerr << #x << ": " << x << " | " << #y << ": " << y << endl;
+#define trace3(x, y, z)              cerr << #x << ": " << x << " | " << #y << ": " << y << " | " << #z << ": " << z << endl;
+#define trace4(a, b, c, d)           cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << endl;
+#define trace5(a, b, c, d, e)        cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << " | " << #e << ": " << e << endl;
+#define trace6(a, b, c, d, e, f)     cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << " | " << #e << ": " << e << " | " << #f << ": " << f << endl;
+#else
+#define trace1(x)
+#define trace2(x, y)
+#define trace3(x, y, z)
+#define trace4(a, b, c, d)
+#define trace5(a, b, c, d, e)
+#define trace6(a, b, c, d, e, f)
+#endif
+
+//dirextion
+const int fx[4][2] = {{0,1}, {0,-1}, {1,0}, {-1,0}};
+const int fxx[8][2] = {{0,1}, {0,-1}, {1,0}, {-1,0}, {1,1}, {1,-1}, {-1,1}, {-1,-1}};
+
+// datatypes
+#define ll long long
+#define ull unsigned long long
+#define ui unsigned int
+#define us unsigned short
+
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+typedef pair< pii, int> piii;
+typedef vector< piii > vpiii;
+typedef pair<int, bool> pib;
+typedef vector< pii > vpii;
+typedef vector< pib > vpib;
+
+const int MAXN =  1010;
+const int MAXT = 110;
+int memo[MAXT][MAXN];
+int sm[MAXT][MAXN];
+vi v[MAXT];
+int N,T;
+// Assume full binary tree on every level choose some leaves(which will force their subtrees to disappear) and recurse
+// while choosing choose only those whose upper limit of time has been achieved :) (greedy solution with DP) 
+int DP(int i, int j){
+    j = miN(j, 1000);
+    if(i > T)return 0;
+    else if(memo[i][j] != -1)return memo[i][j];
+    else{
+        memo[i][j] = 0;
+        forall(k, 0, N+1){
+            if(k <= j)memo[i][j] = maX(memo[i][j], DP(i+1,2*(j-k)) + sm[i][k]);
+        }
+    }
+    return memo[i][j];
+}
+
+int main(){
+    int t,q;
+    s(N);s(T);
+    forall(i, 0, N){
+        s(t);s(q);
+        v[T-t].pb(q);
+    }
+    forall(i, 0, T+1){
+        sm[i][0] = 0;
+        if(v[i].size()>0){
+            sort(all(v[i]));
+            reverse(all(v[i]));
+        }
+        forall(j, 1, MAXN){
+            if(j <= v[i].size()){
+                sm[i][j] = sm[i][j-1] + v[i][j-1];
+            }else{
+                sm[i][j] = 0;
+            }
+        }
+    }
+    fill(memo,-1);
+    printf("%d\n", DP(0,1));
+}
